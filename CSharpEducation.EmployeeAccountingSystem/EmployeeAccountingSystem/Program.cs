340cc789f74e2d4271a23c6e3d14279c473caa22
Aleksei <aleksei.ojegov@yandex.ru>
2025-07-31 11:09:05 +0400

    Update Program.cs

--- CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/Program.cs
+++ CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/Program.cs
@@ -4,11 +4,24 @@ using System;
 
 namespace EmployeeAccountingSystem
 {
+    /// <summary>
+    /// <Класс для старта консольного приложения.
+    /// </summary>
     class Start
     {
+        /// <summary>
+        /// <Класс Main.
+        /// </summary>
         static void Main()
         {
+            /// <summary>
+            /// <Поле для создания объекта класса сотрудника с полной занятостью.
+            /// </summary>
             var fullTimeManager = new EmployeeManager<FullTimeEmployee>();
+
+            /// <summary>
+            /// <Поле для создания объекта класса сотрудника с почасовой оплатой.
+            /// </summary>
             var partTimeManager = new EmployeeManager<PartTimeEmployee>();
 
             while (true)
@@ -22,6 +35,9 @@ namespace EmployeeAccountingSystem
                 Console.WriteLine("6. Выйти");
                 Console.Write("Выберите действие: ");
 
+                /// <summary>
+                /// <Поле для записи выбора действия меню с консоли.
+                /// </summary>
                 string choice = Console.ReadLine();
 
                 switch (choice)
@@ -51,15 +67,30 @@ namespace EmployeeAccountingSystem
             }
         }
 
+        /// <summary>
+        /// <Метод для добавления сотрудника с полной занятостью.
+        /// </summary>
+        /// <param name="manager">Ссылка на класс менеджера сотрудников с полной занятостью.</param>
         private static void AddFullTimeEmployee(EmployeeManager<FullTimeEmployee> manager)
         {
             Console.Write("Имя сотрудника: ");
+            
+            /// <summary>
+            /// <Поле для записи имени сотрудника с консоли.
+            /// </summary>
             string name = Console.ReadLine();
 
             Console.Write("Зарплата сотрудника, целым числом: ");
             try
             {
+                /// <summary>
+                /// <Поле для записи зарплаты сотрудника с консоли.
+                /// </summary>
                 int salary = Convert.ToInt32(Console.ReadLine());
+
+                /// <summary>
+                /// <Поле для создания объекта класса сотрудника с полной занятостью.
+                /// </summary>
                 var employee = new FullTimeEmployee { Name = name, BaseSalary = salary };
                 manager.Add(employee);
 
@@ -71,19 +102,38 @@ namespace EmployeeAccountingSystem
             }
         }
 
+        /// <summary>
+        /// <Метод для добавления сотрудника с почасовой оплатой.
+        /// </summary>
+        /// <param name="manager">Ссылка на класс менеджера сотрудников с почасовой оплатой.</param>
         private static void AddPartTimeEmployee(EmployeeManager<PartTimeEmployee> manager)
         {
             Console.Write("Имя сотрудника: ");
+            
+            /// <summary>
+            /// <Поле для записи имени сотрудника с консоли.
+            /// </summary>
             string name = Console.ReadLine();
 
             try
             {
                 Console.Write("Колличество рабочих часов часов: ");
+
+                /// <summary>
+                /// <Поле для записи колличества отрадотаных часов сотрудником с консоли.
+                /// </summary>
                 int hoursWorked = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Плата за час: ");
+
+                /// <summary>
+                /// <Поле для записи платы за час отработанный сотрудником с консоли.
+                /// </summary>
                 int hourlyRate = Convert.ToInt32(Console.ReadLine());
 
+                /// <summary>
+                /// <Поле для создания объекта класса сотрудника с почасовой оплатой.
+                /// </summary>
                 var employee = new PartTimeEmployee
                 {
                     Name = name,
@@ -100,9 +150,18 @@ namespace EmployeeAccountingSystem
             }
         }
 
+        /// <summary>
+        /// <Метод для получения информации о сотруднике.
+        /// </summary>
+        /// <param name="fullTimeManager">Ссылка на класс менеджера сотрудников с полной занятостью.</param>
+        /// <param name="partTimeManager">Ссылка на класс менеджера сотрудников с почасовой оплатой.</param>
         private static void GetInformationEmployee(EmployeeManager<FullTimeEmployee> fullTimeManager, EmployeeManager<PartTimeEmployee> partTimeManager)
         {
             Console.Write("Имя сотрудника: ");
+
+            /// <summary>
+            /// <Поле для записи имени сотрудника с консоли.
+            /// </summary>
             string name = Console.ReadLine();
 
             Console.Write("Введите '1' если сотрудник полный,\n" +
@@ -110,16 +169,27 @@ namespace EmployeeAccountingSystem
             
             try
             {
+                /// <summary>
+                /// <Поле для выбора типа сотрудника с консоли.
+                /// </summary>
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
+                        /// <summary>
+                        /// <Поле для записи объекта класса сотрудника с полной занятостью из метода класса менеджера сотрудников.
+                        /// </summary>
                         var fullTimeEmployee = fullTimeManager.Get(name);
+                        
                         Console.WriteLine($"Полный сотрудник: Имя - {fullTimeEmployee.Name}, зарплата - {fullTimeEmployee.CalculateSalary()}");
                         break;
                     case "2":
+                        /// <summary>
+                        /// <Поле для записи объекта класса сотрудника с почасовой оплатой из метода класса менеджера сотрудников.
+                        /// </summary>
                         var partTimeEmployee = partTimeManager.Get(name);
+                        
                         Console.WriteLine($"Частичный сотрудник: Имя - {partTimeEmployee.Name}, зарплата - {partTimeEmployee.CalculateSalary()}");
                         break;
                     default:
@@ -137,6 +207,10 @@ namespace EmployeeAccountingSystem
         private static void UpdateEmployeeData(EmployeeManager<FullTimeEmployee> fullTimeManager, EmployeeManager<PartTimeEmployee> partTimeManager)
         {
             Console.Write("Имя сотрудника: ");
+
+            /// <summary>
+            /// <Поле для записи имени сотрудника с консоли.
+            /// </summary>
             string name = Console.ReadLine();
 
             Console.Write("Введите '1' если сотрудник полный,\n" +
@@ -144,11 +218,17 @@ namespace EmployeeAccountingSystem
 
             try
             {
+                /// <summary>
+                /// <Поле для выбора типа сотрудника с консоли.
+                /// </summary>
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
+                        /// <summary>
+                        /// <Поле для записи объекта класса сотрудника с полной занятостью из метода класса менеджера сотрудников.
+                        /// </summary>
                         var fullTimeEmployee = fullTimeManager.Get(name);
 
                         Console.Write("Введите новую зарплату: ");
@@ -159,6 +239,9 @@ namespace EmployeeAccountingSystem
                         break;
 
                     case "2":
+                        /// <summary>
+                        /// <Поле для записи объекта класса сотрудника с почасовой оплатой из метода класса менеджера сотрудников.
+                        /// </summary>
                         var partTimeEmployee = partTimeManager.Get(name);
 
                         Console.Write("Введите новое количество рабочих часов: ");
@@ -185,6 +268,10 @@ namespace EmployeeAccountingSystem
         private static void RemoveEmployee(EmployeeManager<FullTimeEmployee> fullTimeManager, EmployeeManager<PartTimeEmployee> partTimeManager)
         {
             Console.Write("Имя сотрудника: ");
+
+            /// <summary>
+            /// <Поле для записи имени сотрудника с консоли.
+            /// </summary>
             string name = Console.ReadLine();
 
             Console.Write("Введите '1' если сотрудник полный,\n" +
@@ -192,6 +279,9 @@ namespace EmployeeAccountingSystem
 
             try
             {
+                /// <summary>
+                /// <Поле для выбора типа сотрудника с консоли.
+                /// </summary>
                 string choice = Console.ReadLine();
 
                 switch (choice)
