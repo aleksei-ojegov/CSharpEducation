977641381b552dca5b552cb15614aa0cd206d68a
Aleksei <aleksei.ojegov@yandex.ru>
2025-07-31 09:26:35 +0400

    Update IEmployeeManager.cs

--- CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/IEmployeeManager.cs
+++ CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/IEmployeeManager.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace EmployeeAccountingSystem
 {
+    /// <summary>
+    /// <Интрефейс для управления сотрудниками.
+    /// </summary>
     public interface IEmployeeManager<T> where T : Employee
     {
+        /// <summary>
+        /// <Метод для добавления сотрудника.
+        /// </summary>
+        /// <param name="employee">Объект класса сотрудника.</param>
         void Add(T employee);
+
+        /// <summary>
+        /// <Метод для вывода сотрудника по его имени.
+        /// </summary>
+        /// <param name="name">Имя сотрудника.</param>
+        /// <returns>Возращает объект сордника, иначе исключение.</returns>
         T Get(string name);
+
+        /// <summary>
+        /// <Метод для обновления сотрудника.
+        /// </summary>
+        /// <param name="employee">Объект класса сотрудника.</param>
         void Update(T employee);
+
+        /// <summary>
+        /// <Метод для удаления сотрудника.
+        /// </summary>
+        /// <param name="name">Имя сотрудника.</param>
         void Remove(string name);
     }
 }
