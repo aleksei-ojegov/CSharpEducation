795e3cb5f61e60db9f49de0b03953e09f6dd4471
aleksei-ojegov <aleksei.ojegov@yandex.ru>
2025-07-31 23:03:53 +0400

    start

---
+++ Phonebook-master/Phonebook.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phonebook;
+using System;
+using System.Collections.Generic;
+
+namespace Phonebook.Tests
+{
+    [TestClass]
+    public class PhoneBook
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            phoneBook = new PhoneBook();
+        }
+    }
+}
