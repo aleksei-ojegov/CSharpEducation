aae389738175584a7efa57cc509cf7821b732c9b
Aleksei <aleksei.ojegov@yandex.ru>
2025-07-31 08:47:38 +0400

    Update Employee.cs

--- CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/Employee.cs
+++ CSharpEducation.EmployeeAccountingSystem/EmployeeAccountingSystem/Employee.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace EmployeeAccountingSystem
 {
+ /// <summary>
+ /// <Базовый класс для создание работника.
+ /// </summary>
     public abstract class Employee
     {
+        /// <summary>
+        /// <Свойство для имени работника.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// <Свойство для зарплаты работника.
+        /// </summary>
         public decimal BaseSalary { get; set; }
 
+        /// <summary>
+        /// <Метод для вычисления зарплаты работника.
+        /// </summary>
         public abstract decimal CalculateSalary();
     }
 }
